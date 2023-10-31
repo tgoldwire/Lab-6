6a6ad05f45585a031d9d03a0dbb15fa82cd56076
tgoldwire <141956718+tgoldwire@users.noreply.github.com>
2023-10-31 19:41:53 -0400

    Add files via upload
    new code with functions

--- lab6.py
+++ lab6.py
@@ -1,7 +1,12 @@
-number = input('Enter a number: ')
-encode = ''
-for i in number:
-    i = int(i) + 3
-    encode += str(i)
-print('Encoded number:', encode)
-print("tesat")
+def encode(number):
+    encode = ''
+    for i in number:
+        i = int(i) + 3
+        encode += str(i)
+    print('Encoded number:', encode)
+
+
+
+if __name__ == '__main__':
+    number = input('Enter a number: ')
+    encode(number)
